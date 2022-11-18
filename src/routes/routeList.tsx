a6c2b6da02c66784a40905e896e043c503797531
Mrudul6 <99995565+Mrudul6@users.noreply.github.com>
2022-11-18 16:09:19 -0500

    Add files via upload

---
+++ src/routes/routeList.tsx
@@ -0,0 +1,44 @@
+import Home from "../pages/home/Home";
+import Listing from "../pages/lisitng/Listing";
+import Login from "../pages/login/Login";
+import ProductDetails from "../pages/productDetails/ProductDetails";
+import Order from "../pages/order/Order";
+import Cart from "../pages/cart/Cart";
+import * as types from "../types";
+import ROUTE_PATHS from "./routesPath";
+
+
+const routes: types.route[] = [
+  {
+    path: ROUTE_PATHS.LOGIN,
+    component: <Login />,
+    exact: true,
+  },
+  {
+    path: ROUTE_PATHS.LISTING,
+    component: <Listing />,
+    exact: true,
+  },
+  {
+    path: ROUTE_PATHS.HOME,
+    component: <Home />,
+    exact: true,
+  },
+  {
+    path: ROUTE_PATHS.PRODUCT_DETAILS,
+    component: <ProductDetails />,
+    exact: true,
+  },
+  {
+    path: ROUTE_PATHS.ORDER,
+    component: <Order />,
+    exact: true,
+  },
+  {
+    path: ROUTE_PATHS.CART,
+    component: <Cart />,
+    exact: true,
+  },
+];
+
+export default routes;
---
+++ src/routes/routesPath.tsx
@@ -0,0 +1,10 @@
+const ROUTE_PATHS = {
+  HOME: "/",
+  LOGIN: "/login",
+  LISTING: "/listing",
+  PRODUCT_DETAILS: "/product-details",
+  ORDER: "/order",
+  CART: "/cart",
+};
+
+export default ROUTE_PATHS;
