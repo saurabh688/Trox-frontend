1a43043709294b7945a03c8e420623b977e18d16
saurabhrai2834 <saurabh.rai@pace.edu>
2022-12-16 12:20:35 -0500

    update

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./Cart.css";
@@ -14,31 +15,32 @@ const Cart = () => {
 
   return (
     <div className="shopping-cart">
-      <div className="title">Shopping Bag</div>
+      <div style={{ display: "inline-block" }}>
+        <h5 className="cart-title" style={{ display: "inline-block" }}>
+          Shopping Bag
+        </h5>
+        <Button
+          type="submit"
+          variant="contained"
+          style={{ display: "inline-block", textAlign: "right" }}
+        >
+          Continue
+        </Button>
+      </div>
+      <br />
       {cartData.map((item: any) => (
         <div className="item">
-          <div className="buttons">
-            <span className="delete-btn"></span>
-            <span className="like-btn"></span>
-          </div>
-
-          <div className="image">
-            <img width={"50px"} src="https://m.media-amazon.com/images/I/61O9tWR6WDS._SX522_.jpg" alt="productimage" />
+          <div className="cart-image">
+            <img
+              width="200px"
+              src="https://m.media-amazon.com/images/I/61O9tWR6WDS._SX522_.jpg"
+              alt="productimage"
+            />
           </div>
 
           <div className="description">
-            <span>{item.title}</span>
+            <h3>{item.title}</h3>
             <span>{item.description}</span>
-          </div>
-
-          <div className="quantity">
-            <button className="plus-btn" type="button" name="button">
-              <img src="plus.svg" alt="" />
-            </button>
-            <input type="text" name="name" value="1" />
-            <button className="minus-btn" type="button" name="button">
-              <img src="minus.svg" alt="" />
-            </button>
           </div>
 
           <div className="total-price">{`$ ${item.price}`}</div>
